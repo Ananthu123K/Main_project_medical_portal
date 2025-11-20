0abc558aab597ce4c80bdeb58490c90a1eab5a37
Ananthu123K <ananthumadhu29@gmail.com>
2025-11-21 01:08:57 +0530

    updated project

--- webapp/urls.py
+++ webapp/urls.py
@@ -5,7 +5,7 @@ urlpatterns=[
     path('Home/',views.home_page,name="Home"),
     path('Services/',views.service_page,name="Services"),
     path('Donors/',views.donors_page,name="Donors"),
-
-
+    path('Contact/',views.contact_page,name="Contact"),
+    path('About/',views.about_page,name="About"),
 
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from admin_panel.models import ServiceCategoryDb,BloodCategory
 
 # Create your views here.
 
 def home_page(request):
-    return render(request,"Home.html")
+    services=ServiceCategoryDb.objects.all()
+    return render(request,"Home.html",{"services":services})
 def service_page(request):
     return render(request,"Services.html")
 def donors_page(request):
-    return render(request,"Donors.html")
+    services = ServiceCategoryDb.objects.all()
+    return render(request,"Donors.html",{"services":services})
+def contact_page(request):
+    return render(request,"Contact.html")
+def about_page(request):
+    return render(request,"About.html")
